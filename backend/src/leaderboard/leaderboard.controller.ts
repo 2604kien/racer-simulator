09eb573d4c84a36853b625b64626cacc8bb13a28
DESKTOP-F3BMJTI\nguye <hngu0053@student.monash.edu>
2023-12-16 01:10:35 +1100

    finish backend in nestjs

--- backend/src/leaderboard/leaderboard.controller.ts
+++ backend/src/leaderboard/leaderboard.controller.ts
@@ -1,10 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get } from '@nestjs/common';
 import { CreateRaceDto } from './dto/createRaceDto';
 import { LeaderboardService } from './leaderboard.service';
 
 @Controller('leaderboard')
 export class LeaderboardController {
     constructor(private leaderboard:LeaderboardService){}
+    @Get()
+    getLeaderboard(){
+        return this.leaderboard.getLeaderBoard();
+    }
     @Post()
     addRaceToLeaderBoard(@Body() race: CreateRaceDto){
         return this.leaderboard.addRaceToLeaderBoard(race);
--- backend/src/leaderboard/leaderboard.service.ts
+++ backend/src/leaderboard/leaderboard.service.ts
@@ -14,4 +14,13 @@ export class LeaderboardService {
             console.log(error);
         }
     }
+
+    async getLeaderBoard(){
+        try{
+            return this.leaderboard.length>0? {message:"Get leaderboard information successfully", data: this.leaderboard}:{message: "Leaderboard length is 0", data:"There is no race history"};
+        }
+        catch(error){
+            console.log(error)
+        }
+    }
 }
